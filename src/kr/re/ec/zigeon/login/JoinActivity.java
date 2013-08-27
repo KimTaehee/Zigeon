7511bd0e868dbd7fad5b3299105a3a74b7ac2917
wlghd1016 <wlghd1016@naver.com>
2013-08-27 23:30:03 +0900

    commit

--- src/kr/re/ec/zigeon/login/JoinActivity.java
+++ src/kr/re/ec/zigeon/login/JoinActivity.java
@@ -1,4 +1,4 @@
-package kr.re.ec.zigeon.login;
+﻿package kr.re.ec.zigeon.login;
 
 import kr.re.ec.zigeon.R;
 import kr.re.ec.zigeon.handler.SoapParser;
@@ -46,71 +46,71 @@ public class JoinActivity extends Activity implements OnClickListener{
 			LogUtil.v("ID check.compareTo(null) = " + ID_check.compareTo(""));
 			String nicknameQuery = "SELECT memPW FROM tMember WHERE memNick ='" + nickname.getText().toString() + "'";
 			Nickname_check = soapParser.sendQuery(nicknameQuery); 
-			if(id.getText().toString().compareTo("") == 0) { //내용없으면 에러띄우고 강제 return
+			if(id.getText().toString().compareTo("") == 0) { //if have no text, show alert and return
 					
 				AlertDialog.Builder alert = new AlertDialog.Builder(this);
-				alert.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+				alert.setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
 					@Override
 					public void onClick(DialogInterface dialog, int which) {
 						dialog.dismiss();	
 					}
 				});
-				alert.setMessage("id를 입력하세요");
+				alert.setMessage("Input ID.");
 				alert.show();
 				return;
-			} else if(ID_check.compareTo("") != 0)//이미 아이디가 있을때
+			} else if(ID_check.compareTo("") != 0)//duplicated id
 			{
 				AlertDialog.Builder alert = new AlertDialog.Builder(this);
-				alert.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+				alert.setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
 					@Override
 					public void onClick(DialogInterface dialog, int which) {
 						dialog.dismiss();	
 					}
 				});
-				alert.setMessage("이미 사용중인 아이디입니다.");
+				alert.setMessage("This ID already in use.");
 				alert.show();
 				return;
 			}	
 			else if(password.getText().toString().compareTo("") == 0){
 			
 				AlertDialog.Builder alert = new AlertDialog.Builder(this);
-				alert.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+				alert.setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
 					@Override
 					public void onClick(DialogInterface dialog, int which) {
 						dialog.dismiss();	
 					}
 				});
-				alert.setMessage("password를 입력하세요");
+				alert.setMessage("Input Password.");
 				alert.show();
 				return;
 			}else if(nickname.getText().toString().compareTo("") == 0){
 				
 				AlertDialog.Builder alert = new AlertDialog.Builder(this);
-				alert.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+				alert.setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
 					@Override
 					public void onClick(DialogInterface dialog, int which) {
 						dialog.dismiss();	
 					}
 				});
-				alert.setMessage("Nickname을 입력하세요");
+				alert.setMessage("Input Nickname");
 				alert.show();
 				return;
 			}else if(Nickname_check.compareTo("") != 0)
 			{
 				AlertDialog.Builder alert = new AlertDialog.Builder(this);
-				alert.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+				alert.setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
 					@Override
 					public void onClick(DialogInterface dialog, int which) {
 						dialog.dismiss();	
 					}
 				});
-				alert.setMessage("이미 사용중인 닉네임입니다.");
+				alert.setMessage("This Nickname already in use.");
 				alert.show();
 				return;
 			}
 			else{
 			
-				String str = soapParser.sendQuery("SELECT MAX(memIdx) FROM tMember"); //정상작동 확인. +1한 idx로 insert한다.
+				String str = soapParser.sendQuery("SELECT MAX(memIdx) FROM tMember"); //+1 idx insert
 				LogUtil.v(str);
 				int maxMemIdx = Integer.parseInt(str);
 				LogUtil.v("start sendQuery ");
@@ -132,7 +132,7 @@ public class JoinActivity extends Activity implements OnClickListener{
 				break;
 			}
 		}
-		Toast toast = Toast.makeText(this, "회원가입이 되셨습니다.",Toast.LENGTH_SHORT); 
+		Toast toast = Toast.makeText(this, "Welcome to join us!",Toast.LENGTH_SHORT); 
 		toast.show(); 
 		finish();
 	}
