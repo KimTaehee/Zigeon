9a3749927b4b86b25d77a93404bffd41d87c18c4
wlghd1016 <wlghd1016@naver.com>
2013-09-04 00:01:17 +0900

    test... why not? SSANG

---
+++ src/kr/re/ec/zigeon/TestActivity.java
@@ -0,0 +1,31 @@
+package kr.re.ec.zigeon;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+public class TestActivity extends PreferenceActivity{
+/*
+	private Button testBtn;
+	private Intent mIntent;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_test);
+		
+		testBtn = (Button)findViewById(R.id.test_btn1);
+		testBtn.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				mIntent = new Intent(TestActivity.this, PreferenceActivity.class);
+				startActivity(mIntent);
+			}
+		});
+	}*/
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		addPreferencesFromResource(R.layout.activity_preference);
+	}
+}
