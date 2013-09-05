813848012a208e1c538df4fc8a0639b8a3029a85
wlghd1016 <wlghd1016@naver.com>
2013-09-06 01:08:21 +0900

    preference data load test OK

--- src/kr/re/ec/zigeon/TestActivity.java
+++ src/kr/re/ec/zigeon/TestActivity.java
@@ -16,14 +16,19 @@ public class TestActivity extends Activity {
 
 	private Intent intent;
 	private Button testBtn;
+<<<<<<< HEAD
 	private Button testBtn2;
 	private SharedPreferences pref;
 	private PreferenceDataset preference;
+=======
+	private SharedPreferences pref;
+>>>>>>> preference data load test OK
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_test);
 		
+<<<<<<< HEAD
 		testBtn = (Button) findViewById(R.id.test_btn1);
 		testBtn2 = (Button) findViewById(R.id.test_btn2);
 		
@@ -67,6 +72,41 @@ public class TestActivity extends Activity {
 			}
 		});
 
+=======
+		/*
+		getFragmentManager()
+				.beginTransaction()
+				.replace(android.R.id.content,
+						new kr.re.ec.zigeon.PreferenceFrag()).commit();
+*/
+		
+		
+		PreferenceManager.setDefaultValues(TestActivity.this, R.xml.preference,
+				false);
+		  LogUtil.v("1");
+		  testBtn = (Button)findViewById(R.id.test_btn1);
+		  LogUtil.v("2");
+		  pref = PreferenceManager.getDefaultSharedPreferences(TestActivity.this);
+		  LogUtil.v("test=" + pref.getBoolean("preference_CheckBox_notificationSound", false));
+		  if(pref.getBoolean("preference_CheckBox_notificationSound", false))
+		  {
+			  LogUtil.v("3");
+			  testBtn.setText("Checked");
+		  }else
+		  {
+			  testBtn.setText("not Checked");
+		  }
+		  LogUtil.v("5");
+		  testBtn.setOnClickListener(new OnClickListener() {
+		  
+		  @Override public void onClick(View v) {
+			  intent = new Intent(TestActivity.this,
+						PreferenceActivity.class);
+				startActivity(intent);
+		 
+		  } });
+		 
+>>>>>>> preference data load test OK
 	}
 	/*
 	 * @Override protected void onCreate(Bundle savedInstanceState) {
