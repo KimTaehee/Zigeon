be515137eb45f16ba7cd5dbc31685ca658dab373
wlghd1016 <wlghd1016@naver.com>
2013-09-06 01:46:44 +0900

    data load test2

--- src/kr/re/ec/zigeon/TestActivity.java
+++ src/kr/re/ec/zigeon/TestActivity.java
@@ -10,54 +10,58 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 
-public class TestActivity extends Activity{
+public class TestActivity extends Activity {
 
 	private Intent intent;
 	private Button testBtn;
+	private Button testBtn2;
 	private SharedPreferences pref;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_test);
-		
+
 		/*
-		getFragmentManager()
-				.beginTransaction()
-				.replace(android.R.id.content,
-						new kr.re.ec.zigeon.PreferenceFrag()).commit();
-*/
-		
-		
+		 * getFragmentManager() .beginTransaction()
+		 * .replace(android.R.id.content, new
+		 * kr.re.ec.zigeon.PreferenceFrag()).commit();
+		 */
+
 		PreferenceManager.setDefaultValues(TestActivity.this, R.xml.preference,
 				false);
-		  LogUtil.v("1");
-		  testBtn = (Button)findViewById(R.id.test_btn1);
-		  LogUtil.v("2");
-		  pref = PreferenceManager.getDefaultSharedPreferences(TestActivity.this);
-		  LogUtil.v("test=" + pref.getBoolean("preference_CheckBox_notificationSound", false));
-		  if(pref.getBoolean("preference_CheckBox_notificationSound", false))
-		  {
-			  LogUtil.v("3");
-			  testBtn.setText("Checked");
-		  }else
-		  {
-			  testBtn.setText("not Checked");
-		  }
-		  LogUtil.v("5");
-		  testBtn.setOnClickListener(new OnClickListener() {
-		  
-		  @Override public void onClick(View v) {
-			  intent = new Intent(TestActivity.this,
-						PreferenceActivity.class);
+		testBtn = (Button) findViewById(R.id.test_btn1);
+		testBtn2 = (Button) findViewById(R.id.test_btn2);
+		pref = PreferenceManager.getDefaultSharedPreferences(this);
+		if (pref.getBoolean("preference_CheckBox_notificationSound", false)) {
+			testBtn.setText("Checked");
+		} else {
+			testBtn.setText("not Checked");
+		}
+		testBtn.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				intent = new Intent(TestActivity.this, PreferenceActivity.class);
 				startActivity(intent);
-		 
-		  } });
-		 
+
+			}
+		});
+		
+		testBtn2.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				intent = new Intent(TestActivity.this, TestActivity2.class);
+				startActivity(intent);
+				
+			}
+		});
+
 	}
-/*	@Override
-	protected void onCreate(Bundle savedInstanceState)
-	{
-		super.onCreate(savedInstanceState);
-		addPreferencesFromResource(R.xml.activity_preference);
-	}*/
+	/*
+	 * @Override protected void onCreate(Bundle savedInstanceState) {
+	 * super.onCreate(savedInstanceState);
+	 * addPreferencesFromResource(R.xml.activity_preference); }
+	 */
 }
