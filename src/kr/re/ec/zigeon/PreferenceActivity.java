aca7200349df02721c8094de35f93c42bf633122
wlghd1016 <wlghd1016@naver.com>
2013-09-10 01:54:55 +0900

    preferenceActivity click event

--- src/kr/re/ec/zigeon/PreferenceActivity.java
+++ src/kr/re/ec/zigeon/PreferenceActivity.java
@@ -3,10 +3,10 @@ package kr.re.ec.zigeon;
 import kr.re.ec.zigeon.dataset.PreferenceDataset;
 import kr.re.ec.zigeon.util.LogUtil;
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
-import android.content.Intent;
-import android.content.SharedPreferences;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceClickListener;
@@ -46,22 +46,25 @@ public class PreferenceActivity extends Activity{
 			
 			// Get the custom preference
 			Preference Pref_myInfo = (Preference) findPreference(PreferenceDataset.MY_INFO);
-			
+			Preference Pref_PwChange = (Preference) findPreference(PreferenceDataset.PW_CHANGE);
 			// preference set ClickListncer
 			Pref_myInfo.setOnPreferenceClickListener(this);
-
+			Pref_PwChange.setOnPreferenceClickListener(this);
 		}
 		
 		public boolean onPreferenceClick(Preference preference) {
-			  
+			  LogUtil.v("PW test= " + preference.getKey().equals(PreferenceDataset.PW_CHANGE));
 			//preference.getKey().equals("key")
 	        if (preference.getKey().equals(PreferenceDataset.MY_INFO)) {
 	        	Toast.makeText(getBaseContext(),
-						"The custom preference has been clicked",
+						"The custom preference_My_info has been clicked",
 						Toast.LENGTH_LONG).show();
 
-	        } else if (preference.getKey().equals(PreferenceDataset.CHECKBOX_BUBBLEHEAD)) {
-
+	        } else if (preference.getKey().equals(PreferenceDataset.PW_CHANGE)) {
+	        	Toast.makeText(getBaseContext(),
+						"The custom preference_PW_Change has been clicked",
+						Toast.LENGTH_LONG).show();
+	        	DialogSimple();
 	        } else if (preference.getKey().equals("priority")) {
 
 	        } else {
@@ -71,5 +74,27 @@ public class PreferenceActivity extends Activity{
 	    }
 	}
 	
+	private void DialogSimple(){
+	    AlertDialog.Builder alt_bld = new AlertDialog.Builder(this);
+	    alt_bld.setMessage("Do you want to close this window ?").setCancelable(
+	        false).setPositiveButton("Yes",
+	        new DialogInterface.OnClickListener() {
+	        public void onClick(DialogInterface dialog, int id) {
+	            // Action for 'Yes' Button
+	        }
+	        }).setNegativeButton("No",
+	        new DialogInterface.OnClickListener() {
+	        public void onClick(DialogInterface dialog, int id) {
+	            // Action for 'NO' Button
+	            dialog.cancel();
+	        }
+	        });
+	    AlertDialog alert = alt_bld.create();
+	    // Title for AlertDialog
+	    alert.setTitle("Title");
+	    // Icon for AlertDialog
+	    alert.setIcon(R.drawable.icon);
+	    alert.show();
+	}
 	
 }
