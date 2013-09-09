b4a66777d26838a799f3bfc038fee98ce1258921
wlghd1016 <wlghd1016@naver.com>
2013-09-10 00:40:25 +0900

    preferenceDataset complete

--- src/kr/re/ec/zigeon/PreferenceActivity.java
+++ src/kr/re/ec/zigeon/PreferenceActivity.java
@@ -1,5 +1,6 @@
 package kr.re.ec.zigeon;
 
+import kr.re.ec.zigeon.dataset.PreferenceDataset;
 import kr.re.ec.zigeon.util.LogUtil;
 import android.app.Activity;
 import android.app.FragmentManager;
@@ -13,9 +14,8 @@ import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
 import android.widget.Toast;
 
-public class PreferenceActivity extends Activity {
-	private Intent intent;
-	
+public class PreferenceActivity extends Activity{
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -29,48 +29,47 @@ public class PreferenceActivity extends Activity {
 
 		getFragmentManager().beginTransaction()
 				.replace(android.R.id.content, new PrefsFragment()).commit();
-		PreferenceManager.setDefaultValues(PreferenceActivity.this, R.xml.preference,
+		PreferenceManager.setDefaultValues(PreferenceActivity.this, R.xml.preference, 
 				false);
+		
+		
 
 	}
 
-	public class PrefsFragment extends PreferenceFragment {
+	public class PrefsFragment extends PreferenceFragment implements OnPreferenceClickListener{
 
 		@Override
 		public void onCreate(Bundle savedInstanceState) {
 
 			super.onCreate(savedInstanceState);
-			LogUtil.v("4");
 			addPreferencesFromResource(R.xml.preference);
-
+			
 			// Get the custom preference
-			Preference Pref_myInfo = (Preference) findPreference("preference_myInfo");
-			Pref_myInfo
-					.setOnPreferenceClickListener(new OnPreferenceClickListener() {
-
-						public boolean onPreferenceClick(Preference preference) {
-							Toast.makeText(getBaseContext(),
-									"The custom preference has been clicked",
-									Toast.LENGTH_LONG).show();
-							/*
-							SharedPreferences customSharedPreference = getSharedPreferences(
-									"myCustomSharedPrefs",
-									Activity.MODE_PRIVATE);
-							SharedPreferences.Editor editor = customSharedPreference
-									.edit();
-							editor.putString("myCustomPref",
-									"The preference has been clicked");
-							editor.commit();*/
-							intent = new Intent(PreferenceActivity.this,
-									TestActivity.class);
-							startActivity(intent);
-							
-							return true;
-						}
-
-					});
+			Preference Pref_myInfo = (Preference) findPreference(PreferenceDataset.MY_INFO);
+			
+			// preference set ClickListncer
+			Pref_myInfo.setOnPreferenceClickListener(this);
 
 		}
+		
+		public boolean onPreferenceClick(Preference preference) {
+			  
+			//preference.getKey().equals("key")
+	        if (preference.getKey().equals(PreferenceDataset.MY_INFO)) {
+	        	Toast.makeText(getBaseContext(),
+						"The custom preference has been clicked",
+						Toast.LENGTH_LONG).show();
 
+	        } else if (preference.getKey().equals(PreferenceDataset.CHECKBOX_BUBBLEHEAD)) {
+
+	        } else if (preference.getKey().equals("priority")) {
+
+	        } else {
+
+	        }
+	        return false;
+	    }
 	}
+	
+	
 }
--- src/kr/re/ec/zigeon/TestActivity.java
+++ src/kr/re/ec/zigeon/TestActivity.java
@@ -1,5 +1,6 @@
 package kr.re.ec.zigeon;
 
+import kr.re.ec.zigeon.dataset.PreferenceDataset;
 import kr.re.ec.zigeon.util.LogUtil;
 import android.app.Activity;
 import android.content.Intent;
@@ -16,28 +17,35 @@ public class TestActivity extends Activity {
 	private Button testBtn;
 	private Button testBtn2;
 	private SharedPreferences pref;
-
+	private PreferenceDataset preference;
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_test);
-
-		/*
-		 * getFragmentManager() .beginTransaction()
-		 * .replace(android.R.id.content, new
-		 * kr.re.ec.zigeon.PreferenceFrag()).commit();
-		 */
-
-		PreferenceManager.setDefaultValues(TestActivity.this, R.xml.preference,
-				false);
+		
 		testBtn = (Button) findViewById(R.id.test_btn1);
 		testBtn2 = (Button) findViewById(R.id.test_btn2);
+		
+		preference = PreferenceDataset.getInstance(this);
+		preference.setPreference(this);
+		
+		if(preference.sharedPref.getBoolean(PreferenceDataset.CHECKBOX_SOUND,false))
+		{
+			testBtn.setText("Checked");
+		} else {
+			testBtn.setText("not Checked");	
+		}
+		
+		
+		/*
+		PreferenceManager.setDefaultValues(TestActivity.this, R.xml.preference,
+				false);
 		pref = PreferenceManager.getDefaultSharedPreferences(this);
 		if (pref.getBoolean("preference_CheckBox_notificationSound", false)) {
 			testBtn.setText("Checked");
 		} else {
 			testBtn.setText("not Checked");
-		}
+		}*/
 		testBtn.setOnClickListener(new OnClickListener() {
 
 			@Override
