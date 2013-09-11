6dc8c26dab78cb184571a6e18d5c92f2a02af0f0
wlghd1016 <wlghd1016@naver.com>
2013-09-11 19:19:55 +0900

    commit

--- src/kr/re/ec/zigeon/PreferenceActivity.java
+++ src/kr/re/ec/zigeon/PreferenceActivity.java
@@ -14,7 +14,7 @@ import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
 import android.widget.Toast;
 
-public class PreferenceActivity extends Activity{
+public class PreferenceActivity extends Activity {
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -22,28 +22,27 @@ public class PreferenceActivity extends Activity{
 		FragmentManager mFragmentManager = getFragmentManager();
 		FragmentTransaction mFragmentTransaction = mFragmentManager
 				.beginTransaction();
-		
+
 		PrefsFragment mPrefsFragment = new PrefsFragment();
 		mFragmentTransaction.replace(android.R.id.content, mPrefsFragment);
 		mFragmentTransaction.commit();
 
 		getFragmentManager().beginTransaction()
-				.replace(android.R.id.content, new PrefsFragment()).commit();
-		PreferenceManager.setDefaultValues(PreferenceActivity.this, R.xml.preference, 
-				false);
-		
-		
+		.replace(android.R.id.content, new PrefsFragment()).commit();
+		PreferenceManager.setDefaultValues(PreferenceActivity.this,
+				R.xml.preference, false);
 
 	}
 
-	public class PrefsFragment extends PreferenceFragment implements OnPreferenceClickListener{
+	public class PrefsFragment extends PreferenceFragment implements
+	OnPreferenceClickListener {
 
 		@Override
 		public void onCreate(Bundle savedInstanceState) {
 
 			super.onCreate(savedInstanceState);
 			addPreferencesFromResource(R.xml.preference);
-			
+
 			// Get the custom preference
 			Preference Pref_myInfo = (Preference) findPreference(PreferenceDataset.MY_INFO);
 			Preference Pref_PwChange = (Preference) findPreference(PreferenceDataset.PW_CHANGE);
@@ -51,50 +50,52 @@ public class PreferenceActivity extends Activity{
 			Pref_myInfo.setOnPreferenceClickListener(this);
 			Pref_PwChange.setOnPreferenceClickListener(this);
 		}
-		
+
 		public boolean onPreferenceClick(Preference preference) {
-			  LogUtil.v("PW test= " + preference.getKey().equals(PreferenceDataset.PW_CHANGE));
-			//preference.getKey().equals("key")
-	        if (preference.getKey().equals(PreferenceDataset.MY_INFO)) {
-	        	Toast.makeText(getBaseContext(),
+			LogUtil.v("PW test= "
+					+ preference.getKey().equals(PreferenceDataset.PW_CHANGE));
+			// preference.getKey().equals("key")
+			if (preference.getKey().equals(PreferenceDataset.MY_INFO)) {
+				Toast.makeText(getBaseContext(),
 						"The custom preference_My_info has been clicked",
 						Toast.LENGTH_LONG).show();
 
-	        } else if (preference.getKey().equals(PreferenceDataset.PW_CHANGE)) {
-	        	Toast.makeText(getBaseContext(),
+			} else if (preference.getKey().equals(PreferenceDataset.PW_CHANGE)) {
+				Toast.makeText(getBaseContext(),
 						"The custom preference_PW_Change has been clicked",
 						Toast.LENGTH_LONG).show();
-	        	DialogSimple();
-	        } else if (preference.getKey().equals("priority")) {
+				DialogSimple();
+			} else if (preference.getKey().equals("priority")) {
 
-	        } else {
+			} else {
 
-	        }
-	        return false;
-	    }
+			}
+			return false;
+		}
 	}
-	
-	private void DialogSimple(){
-	    AlertDialog.Builder alt_bld = new AlertDialog.Builder(this);
-	    alt_bld.setMessage("Do you want to close this window ?").setCancelable(
-	        false).setPositiveButton("OK",
-	        new DialogInterface.OnClickListener() {
-	        public void onClick(DialogInterface dialog, int id) {
-	            // Action for 'OK' Button
-	        }
-	        }).setNegativeButton("Cancel",
-	        new DialogInterface.OnClickListener() {
-	        public void onClick(DialogInterface dialog, int id) {
-	            // Action for 'Cancel' Button
-	            dialog.cancel();
-	        }
-	        });
-	    AlertDialog alert = alt_bld.create();
-	    // Title for AlertDialog
-	    alert.setTitle("Title");
-	    // Icon for AlertDialog
-	    alert.setIcon(R.drawable.icon);
-	    alert.show();
+
+	private void DialogSimple() {
+		AlertDialog.Builder alt_bld = new AlertDialog.Builder(this);
+		alt_bld.setMessage("Do you want to close this window ?")
+		.setCancelable(false)
+		.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int id) {
+				// Action for 'OK' Button
+			}
+		})
+		.setNegativeButton("Cancel",
+				new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int id) {
+				// Action for 'Cancel' Button
+				dialog.cancel();
+			}
+		});
+		AlertDialog alert = alt_bld.create();
+		// Title for AlertDialog
+		alert.setTitle("Title");
+		// Icon for AlertDialog
+		alert.setIcon(R.drawable.icon);
+		alert.show();
 	}
-	
+
 }
