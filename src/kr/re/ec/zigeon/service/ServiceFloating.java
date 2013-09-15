1d20ee1a4f9ed874397b304f33314d80850fed52
wlghd1016 <wlghd1016@naver.com>
2013-09-15 23:43:31 +0900

    no_hangle?

--- src/kr/re/ec/zigeon/service/ServiceFloating.java
+++ src/kr/re/ec/zigeon/service/ServiceFloating.java
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 package kr.re.ec.zigeon.service;
 
 import kr.re.ec.zigeon.BalloonHeadButtonActivity;
@@ -253,6 +254,8 @@ public class ServiceFloating extends Service implements Runnable{
 	}
 
 =======
+=======
+>>>>>>> no_hangle?
 package kr.re.ec.zigeon.service;
 
 import kr.re.ec.zigeon.BalloonHeadButtonActivity;
@@ -509,5 +512,8 @@ public class ServiceFloating extends Service implements Runnable{
         
 	}
 
+<<<<<<< HEAD
 >>>>>>> ServiceFloating auto service stop
+=======
+>>>>>>> no_hangle?
 }
