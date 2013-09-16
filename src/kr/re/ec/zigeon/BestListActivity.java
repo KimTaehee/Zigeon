ef96fa11cb45d913b6fc52d0561da14e0e79d8a3
KimTaeHee <slhyvaa@nate.com>
2013-09-16 20:25:46 +0900

    top 20 tLandmark & landmark adapter update

--- src/kr/re/ec/zigeon/BestListActivity.java
+++ src/kr/re/ec/zigeon/BestListActivity.java
@@ -92,7 +92,7 @@ public class BestListActivity extends Activity implements OnClickListener {
 				for(int i=0; i<mBestListArr.length; i++) {
 					mBestListArr[i].getDistance(myLocation);	//calc LocationDataset.distanceFromCurrentLocation
 				}
-				
+				LogUtil.i("mBestListArr.length: " + mBestListArr.length);
 				mBestListAdp = new LandmarkAdapter(BestListActivity.this, mBestListArr);
 				grdBestList.setAdapter(mBestListAdp);
 				mBestListAdp.notifyDataSetChanged();	//TODO: is this work?
@@ -136,9 +136,9 @@ public class BestListActivity extends Activity implements OnClickListener {
 
 				//WARN: cannot use this query on UpdateService.onLocationChanged().
 				//WARN: It may cause to send to other Activity.
-				LogUtil.v("select * from tLandmark WHERE ldmVisible = 'True'");
+				LogUtil.v("select TOP 20 * from tLandmark WHERE ldmVisible = 'True'");
 				uiHandler.sendMessage(Constants.MSG_TYPE_LANDMARK, "", 
-						soapParser.getSoapData("select * from tLandmark WHERE ldmVisible = 'True'", Constants.MSG_TYPE_LANDMARK));
+						soapParser.getSoapData("select TOP 20 * from tLandmark WHERE ldmVisible = 'True'", Constants.MSG_TYPE_LANDMARK));
 
 				//String str = myLocation.getLatitude() + "\n" + myLocation.getLongitude() + "\n";
 
@@ -164,9 +164,9 @@ public class BestListActivity extends Activity implements OnClickListener {
 
 		//request contents
 		soapParser = SoapParser.getInstance(); 
-		LogUtil.v("data request. select * from tLandmark WHERE ldmVisible = 'True'");
+		LogUtil.v("data request. select TOP 20 * from tLandmark WHERE ldmVisible = 'True'");
 		uiHandler.sendMessage(Constants.MSG_TYPE_LANDMARK, "", 
-				soapParser.getSoapData("select * from tLandmark WHERE ldmVisible = 'True'", Constants.MSG_TYPE_LANDMARK));
+				soapParser.getSoapData("select TOP 20 * from tLandmark WHERE ldmVisible = 'True'", Constants.MSG_TYPE_LANDMARK));
 		
 
 		/********** init UI ************/
