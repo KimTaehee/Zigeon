25a28703fdcadb0d5d3406224df3b73d9c3d7176
wlghd1016 <wlghd1016@naver.com>
2013-10-03 20:20:10 +0900

    BallonHead service

--- src/kr/re/ec/zigeon/handler/ServiceFloating.java
+++ src/kr/re/ec/zigeon/handler/ServiceFloating.java
@@ -20,10 +20,10 @@ import android.widget.ImageView;
 @SuppressLint("ResourceAsColor")
 public class ServiceFloating extends Service implements Runnable {
 
-	public static int ID_NOTIFICATION = 2018;
+	public static  int ID_NOTIFICATION = 2018;
 
 	private WindowManager windowManager;
-	private ImageView chatHead;
+	private ImageView balloonHead;
 	private ImageView quit;
 	private Button wordBubble;
 
@@ -35,12 +35,15 @@ public class ServiceFloating extends Service implements Runnable {
 	private boolean mRunning;
 	// timer set (5sec)
 	private static final int TIMER_PERIOD = 5 * 1000;
-	private static final int COUNT = 2;
+	private static final int COUNT = 1;
 	private int mCounter;
+
 
 	Activity imsi = new Activity();
 	long lastPressTime;
 	long textFloatTime;
+
+
 
 	@Override
 	public IBinder onBind(Intent intent) {
@@ -48,35 +51,44 @@ public class ServiceFloating extends Service implements Runnable {
 		return null;
 	}
 
-	@Override
+
+	@Override 
 	public void onCreate() {
 		super.onCreate();
-
+		
 		mHandler = new Handler();
 		mRunning = false;
 
+
 		windowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
-		chatHead = new ImageView(this);
+		balloonHead = new ImageView(this);
 		quit = new ImageView(this);
 		wordBubble = new Button(this);
-		chatHead.setImageResource(R.drawable.balloonhead);
-		// wordBubble.setImageResource(R.drawable.wordbubble);
+
+		balloonHead.setImageResource(R.drawable.balloonhead);
+		//		wordBubble.setImageResource(R.drawable.wordbubble);
 		wordBubble.setBackgroundResource(R.drawable.wordbubble);
 		wordBubble.setText(R.string.wordbubble_text);
 		wordBubble.setTextColor(R.color.wordbubble_text);
 		quit.setImageResource(R.drawable.quit);
+
 
 		final WindowManager.LayoutParams wordBubbleParams = new WindowManager.LayoutParams(
 				WindowManager.LayoutParams.WRAP_CONTENT,
 				WindowManager.LayoutParams.WRAP_CONTENT,
 				WindowManager.LayoutParams.TYPE_PHONE,
 				WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
-						| WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+				| WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
 				PixelFormat.TRANSLUCENT);
+
 		wordBubbleParams.gravity = Gravity.TOP | Gravity.LEFT;
-		wordBubbleParams.x = 160;
-		wordBubbleParams.y = 135;
+//		wordBubbleParams.x = (int) balloonHead.getWidth();
+//		wordBubbleParams.y = (int) balloonHead.getHeight() + (balloonHead.getHeight()/4); 
+		wordBubbleParams.x = 150;
+		wordBubbleParams.y = 0;
+
 		windowManager.addView(wordBubble, wordBubbleParams);
+
 
 		final WindowManager.LayoutParams quitParams = new WindowManager.LayoutParams(
 				WindowManager.LayoutParams.WRAP_CONTENT,
@@ -86,56 +98,75 @@ public class ServiceFloating extends Service implements Runnable {
 				PixelFormat.TRANSLUCENT);
 
 		quitParams.gravity = Gravity.BOTTOM | Gravity.CENTER;
-		quitParams.x = 0;
-		quitParams.y = 0;
 
 		windowManager.addView(quit, quitParams);
 
-		final WindowManager.LayoutParams chatHeadParams = new WindowManager.LayoutParams(
+
+
+		final WindowManager.LayoutParams balloonHeadParams = new WindowManager.LayoutParams(
 				WindowManager.LayoutParams.WRAP_CONTENT,
 				WindowManager.LayoutParams.WRAP_CONTENT,
 				WindowManager.LayoutParams.TYPE_PHONE,
 				WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
 				PixelFormat.TRANSLUCENT);
 
-		chatHeadParams.gravity = Gravity.TOP | Gravity.LEFT;
-		chatHeadParams.x = 0;
-		chatHeadParams.y = 100;
+		balloonHeadParams.gravity = Gravity.TOP | Gravity.LEFT;
+		balloonHeadParams.x = 0;
+		balloonHeadParams.y = 0;
 
-		windowManager.addView(chatHead, chatHeadParams);
 
-		// LogUtil.v("chatHead: " + chatHeadParams.x + ", " + chatHeadParams.y);
-		// LogUtil.v("wordbubble: " + wordBubbleParams.x + ", " +
-		// wordBubbleParams.y);
+		windowManager.addView(balloonHead, balloonHeadParams);
+
+
+
+		//		LogUtil.v("balloonHead: " + balloonHeadParams.x + ", " + balloonHeadParams.y);
+		//		LogUtil.v("wordBubble: " + wordBubbleParams.x + ", " + wordBubbleParams.y);
+
+
+
 
 		try {
 			LogUtil.i("try invoked!!");
 
 			quit.setVisibility(View.INVISIBLE);
 
-			chatHead.setOnTouchListener(new View.OnTouchListener() {
-				private WindowManager.LayoutParams paramsF = chatHeadParams;
+
+			balloonHead.setOnTouchListener(new View.OnTouchListener() {
+				private WindowManager.LayoutParams paramsF = balloonHeadParams;
 				private WindowManager.LayoutParams paramsG = wordBubbleParams;
-				private WindowManager.LayoutParams paramsH = quitParams;
+				//				private WindowManager.LayoutParams paramsH = quitParams;
 				private int initialX;
 				private int initialY;
 				private float initialTouchX;
 				private float initialTouchY;
+				private float x;
+				private float y;
+				private int quitWidth;
+				private int quitHeight;
+				private int balloonHeadWidth;
+				private int balloonHeadHeight;
+
+
 
 				int moveCheck = 0;
 
-				@Override
-				public boolean onTouch(View v, MotionEvent event) {
+				@Override public boolean onTouch(View v, MotionEvent event) {
 
-					if (moveCheck == 3) {
+					quitWidth = quit.getWidth();
+					quitHeight = quit.getHeight();
+
+					balloonHeadWidth = balloonHead.getWidth();
+					balloonHeadHeight = balloonHead.getHeight();
+
+
+					if(moveCheck == 3){
 						LogUtil.v("quit visible!");
 						quit.setVisibility(View.VISIBLE);
 					}
 
 					switch (event.getAction()) {
 					case MotionEvent.ACTION_DOWN:
-						LogUtil.v("onTouch_ACTION_DOWN invoked!! movecheck: "
-								+ moveCheck);
+						LogUtil.v("onTouch_ACTION_DOWN invoked!! movecheck: " + moveCheck);
 						initialX = paramsF.x;
 						initialY = paramsF.y;
 						initialTouchX = event.getRawX();
@@ -145,36 +176,41 @@ public class ServiceFloating extends Service implements Runnable {
 					case MotionEvent.ACTION_UP:
 						LogUtil.v("onTouch_ACTION_UP invoked!!");
 
-						if (moveCheck < 3) {
+						x=event.getRawX();
+						y=event.getRawY();
+						LogUtil.i("x= "+x+"   y = "+y);
+
+						int quitCoord[] = new int[2];
+						quit.getLocationOnScreen(quitCoord);
+						LogUtil.i("quit \n x : " + quitCoord[0] + "   y = "+ quitCoord[1]);
+						//						LogUtil.i("\nqC-qW = " +(quitCoord[0]-quitWidth) +"\nqC+2qW = "+(quitCoord[0]+(2*quitWidth)) +
+						//								"\nqC+qH = "+(quitCoord[1]-(quitWidth/2)));
+
+
+						if(moveCheck<3){
 							LogUtil.v("onTouch_ACTION_UP -> click invoked!!");
-							Intent intent = new Intent(getApplicationContext(),
-									BalloonHeadButtonActivity.class);
-							intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK
-									| Intent.FLAG_ACTIVITY_SINGLE_TOP
-									| Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+							Intent intent = new Intent(getApplicationContext(), BalloonHeadButtonActivity.class);
+							intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_SINGLE_TOP|Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
 							getApplicationContext().startActivity(intent);
 						}
 						quit.setVisibility(View.INVISIBLE);
-						if (paramsF.x > 270 && paramsF.y > 90
-								&& paramsF.x < 290 && paramsF.y < 120) {
-							chatHead.setVisibility(View.INVISIBLE);
-							wordBubble.setVisibility(View.INVISIBLE);
+						if(x > (quitCoord[0]-(quitWidth/6)) && x < (quitCoord[0]+quitWidth+(quitWidth/6))
+								&& y > (quitCoord[1]-(quitHeight/6))){
+							stopSelf();
 						}
 						moveCheck = 0;
-						// LogUtil.v("ACTION_UP: movecheck: " + moveCheck);
+						//						LogUtil.v("ACTION_UP: movecheck: " + moveCheck);
 						break;
 
 					case MotionEvent.ACTION_MOVE:
-						// LogUtil.v("onTouch_ACTION_MOVE invoked!!");
-						paramsF.x = initialX
-								+ (int) (event.getRawX() - initialTouchX);
-						paramsF.y = initialY
-								+ (int) (event.getRawY() - initialTouchY);
-						paramsG.x = paramsF.x + 160;
-						paramsG.y = paramsF.y + 35;
+						//LogUtil.v("onTouch_ACTION_MOVE invoked!!");
+						paramsF.x = initialX + (int) (event.getRawX() - initialTouchX);
+						paramsF.y = initialY + (int) (event.getRawY() - initialTouchY);
+						paramsG.x = paramsF.x + balloonHeadWidth;
+						paramsG.y = paramsF.y + (balloonHeadHeight/4);
 
-						LogUtil.v("ParamsF: " + paramsF.x + ", " + paramsF.y);
-						windowManager.updateViewLayout(chatHead, paramsF);
+						//LogUtil.v("ParamsF: " + paramsF.x + ", "+ paramsF.y);
+						windowManager.updateViewLayout(balloonHead, paramsF);
 						windowManager.updateViewLayout(wordBubble, paramsG);
 
 						moveCheck++;
@@ -184,16 +220,18 @@ public class ServiceFloating extends Service implements Runnable {
 					return false;
 				}
 
+
 			}
 
-			);
+
+					);
 
 		} catch (Exception e) {
 			// TODO: handle exception
 		}
 
-	}
 
+	}
 	// call when service start . started in background processing.
 	// startId : Service start request id. stopSelf used to exit from.
 	// onStart is called multiple times,therefore used as an identifier
@@ -222,7 +260,7 @@ public class ServiceFloating extends Service implements Runnable {
 		} else if (--mCounter <= 0) {
 			// connter=0 stopSelf
 			LogUtil.v("stop Service id = " + mStartId);
-			stopSelf(mStartId);
+			wordBubble.setVisibility(View.INVISIBLE);
 		} else {
 			// Require the operation again
 			LogUtil.v("mCounter : " + mCounter);
@@ -230,18 +268,15 @@ public class ServiceFloating extends Service implements Runnable {
 		}
 	}
 
+
 	@Override
 	public void onDestroy() {
 		super.onDestroy();
-		if (chatHead != null)
-			windowManager.removeView(chatHead);
-		if (wordBubble != null)
-			windowManager.removeView(wordBubble);
-		if (quit != null)
-			windowManager.removeView(quit);
-
+		if (balloonHead != null) windowManager.removeView(balloonHead);
+		if (wordBubble != null) windowManager.removeView(wordBubble);
+		if (quit != null) windowManager.removeView(quit);
+		
 		mRunning = false;
-
 	}
-}
 
+}
